f8e8fc88923a0cefb723b07825d6f1134b55a943
UniqueDamsel <catherineadaji@gmail.com>
2025-03-11 16:24:15 +0100

    brief

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
- document.querySelector('header').
- addEventListener('click',function())
- {
-    document.body.classList.toggle('dark-theme');
+ document.querySelector('header').addEventListener('click',function(){
+ 
+    document.querySelector('main').classList.toggle('dark-theme');
+    document.querySelector( 'header').classList.toggle( 'dark-theme');
+    document.querySelector( 'footer').classList.toggle( 'dark-theme');
  })
